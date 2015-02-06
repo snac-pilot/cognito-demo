75e92135698d7cfddf8b33c347044ae471251b40
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-05 20:20:11 -0800

    Updates

---
+++ scripts/d1cca53c.main.js
@@ -0,0 +1,1 @@
+"use strict";hello.on("auth.login",function(a){console.log(a)}),hello.init({google:"593494804152-2jt9r0j9c5qhi04das20f9am6tblh1rq.apps.googleusercontent.com"},{redirect_uri:"http://snac-pilot.github.io/cognito-demo/"});
