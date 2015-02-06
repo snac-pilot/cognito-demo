27faf3538661da9fceff34b912f5505bed006b19
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-06 14:48:07 -0800

    js to pull username from google

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,19 @@ hello.on('auth.login', function(auth){
       'accounts.google.com': auth.authResponse.access_token
     }
   });
-  console.log('You are now logged in.');
+
+  // call user information, for the given network
+  hello( auth.network ).api( '/me' ).then( function(r){
+    // Inject it into the container
+    var label = document.getElementById( 'profile_'+ auth.network );
+    if(!label){
+      label = document.createElement('div');
+      label.id = 'profile_'+auth.network;
+      document.getElementById('profile').appendChild(label);
+    }
+    label.innerHTML = '<img src="'+ r.thumbnail +'" /> Hey '+r.name;
+  });
+
 });
 
 
