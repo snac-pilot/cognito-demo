d5e3805f395a17be2f7d4b5f21c87b559eac6304
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-05 21:09:03 -0800

    AWS sdk js hooked in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,21 @@
 'use strict';
 
 /* global hello */
+/* global AWS */
 /* jshint camelcase: false */
 
 hello.on('auth.login', function(auth){
 
-  // call user information, for the given network
-  hello( auth.network ).api( '/me' ).then( function(r){
-    // Inject it into the container
-    var label = document.getElementById( 'profile_'+ auth.network );
-    if(!label){
-      label = document.createElement('div');
-      label.id = 'profile_'+auth.network;
-      document.getElementById('profile').appendChild(label);
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:2229d0aa-09c2-450d-90da-9cae70b8260f',
+    Logins: { // optional tokens, used for authenticated login
+      'accounts.google.com': auth.authResponse.access_token
     }
-    label.innerHTML = '<img src="'+ r.thumbnail +'" /> Hey '+r.name;
   });
+  console.log('You are now logged in.');
 });
+
+
 hello.init({
   google   : '593494804152-2jt9r0j9c5qhi04das20f9am6tblh1rq.apps.googleusercontent.com'
 },{redirect_uri:'http://snac-pilot.github.io/cognito-demo/'});
