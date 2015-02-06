2dd99870ceb779f5f77bbe1bc3ba920ed03f9a5b
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-05 20:15:12 -0800

    logs in to google

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,5 +18,5 @@ hello.on('auth.login', function(auth){
   });
 });
 hello.init({
-  google   : '...apps.googleusercontent.com'
-},{redirect_uri:'http://localhost:9000/'});
+  google   : '593494804152-2jt9r0j9c5qhi04das20f9am6tblh1rq.apps.googleusercontent.com'
+},{redirect_uri:'http://snac-pilot.github.io/cognito-demo/'});
