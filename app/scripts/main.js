4b923bd525c2b35fb388e337a19da7d269bb933d
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-07 21:58:36 -0800

    back to hello.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* global AWS */
-/* global gapi */
+/* global hello */
 /* global jQuery */
 /* jshint camelcase: false */
 
@@ -66,24 +66,19 @@ var cognitoTestApp = {
 };
 
 
-var loginFinished = function(authResult) {
-if (authResult) {
-  var el = document.getElementById('oauth2-results');
-  var label = '';
-  if (authResult.status.signed_in) {
-    label = 'User granted access:';
-    $('#startFlow').hide();
-    gapi.auth.setToken(authResult);
-  } else {
-    label = 'Access denied: ' + authResult.error;
-  }
-  el.innerHTML = label;
+var loginFinished = function(auth) {
+if (auth) {
+
+  $('#login').hide();
+
+  hello( auth.network ).api( '/me' ).then( function(r){
+    $('#user_info').html('<img src="'+ r.thumbnail +'" /> Hey '+r.name);
+  });
 
   AWS.config.region = 'us-east-1';
-
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'us-east-1:2229d0aa-09c2-450d-90da-9cae70b8260f',
-    Logins: { 'accounts.google.com': authResult.id_token }
+    Logins: { 'accounts.google.com': auth.authResponse.id_token }
   });
 
   AWS.config.credentials.get(function(err) {
@@ -100,6 +95,7 @@ if (authResult) {
         if (! err) {
           // now that we have a dataset we can read and write
           // key/value pairs from it
+          console.log(dataset);
           dataset.get('MyKey', function(err, value) {
             if (! err) {
               cognitoTestApp.testSetup(value, dataset);
@@ -114,12 +110,20 @@ if (authResult) {
 
 jQuery( document ).ready(function( $ ) {
   $('#login').click(function(){
-    gapi.auth.signIn({
-      'callback' : loginFinished,
-      'approvalprompt' : 'force',
-      'clientid' : '593494804152-2jt9r0j9c5qhi04das20f9am6tblh1rq.apps.googleusercontent.com',
-      'requestvisibleactions' : 'http://schema.org/AddAction',
-      'cookiepolicy' : 'single_host_origin'
+    hello( 'google' ).login({
+      response_type : 'code'
     });
   });
 });
+
+hello.init({
+  google   : '593494804152-2jt9r0j9c5qhi04das20f9am6tblh1rq.apps.googleusercontent.com'
+},{
+  redirect_uri:'http://localhost:9000/'
+});
+
+hello.on('auth.login', function(auth){
+  loginFinished(auth);
+});
+
+
